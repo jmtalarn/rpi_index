d43826b8680183c7692f40b807de07b6abfe0a42
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2020-04-08 02:10:21 +0200

    Working on videos list

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
-// import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
 
-const Page = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-`;
-const Title = styled.h1`
-    flex: 1;
-`;
+import { kebabCase } from '../utils/string-utils';
+import FileBox from './FileBox';
+import Grid from './Grid';
+import ErrorMessage from './ErrorMessage';
 
-type VideosProps = Readonly<{}>;
+// eslint-disable-next-line no-unused-vars
+import { FileType } from './FileType';
 
-const Videos: React.SFC<VideosProps> = () => (
-    <Page>
-        <Title>Videos list</Title>
-    </Page>
-);
+type DocsProps = Readonly<{}>;
 
-export default Videos;
+const Docs: React.SFC<DocsProps> = () => {
+    const [hasError, setErrors] = useState(false);
+    const [docs, setDocs]: [FileType[], Function] = useState([]);
+    const path = 'videos';
+
+    useEffect(() => {
+        fetch(`http://192.168.0.22/json/${path}`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res);
+                setDocs(res);
+            })
+            .catch(() => setErrors(true));
+    }, []);
+
+    return (
+        <>
+            {hasError && <ErrorMessage>Something went wrong</ErrorMessage>}
+            {docs.length > 0 && (
+                <Grid>
+                    {docs.map(doc => (
+                        <FileBox key={kebabCase(doc.name)} focusKey={kebabCase(doc.name)} path={path} file={doc} />
+                    ))}
+                </Grid>
+            )}
+        </>
+    );
+};
+
+export default Docs;
